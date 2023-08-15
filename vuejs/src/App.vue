1e46f5386e4b96f314c5b116ac87ecad1792b534
Ekim <ekimbasoglu@hotmail.com>
2023-08-15 13:31:45 +0300

    Vuejs - Init
    To practice vuejs will transform the Angular project to VueJs

---
+++ vuejs/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-nav-bar />
+</template>
+
+<script>
+import navBar from "@/components/Navbar.vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    "app-nav-bar": navBar,
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ vuejs/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <!-- Your home page content goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
